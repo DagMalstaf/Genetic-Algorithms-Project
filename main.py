788e80eb8d75298178ab87230c425a7de11792c5
DagMalstaf <92030435+DagMalstaf@users.noreply.github.com>
2024-10-28 09:48:09 +0100

    wip

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class r0799028:
 
 		tsm = TravellingSalesMan(distanceMatrix, parameters)
 
-		yourConvergenceTestsHere = False
+		yourConvergenceTestsHere = True
 		while( yourConvergenceTestsHere ):
 			# float, float, np.array
 			meanObjective, bestObjective, bestSolution = tsm.run(population)
